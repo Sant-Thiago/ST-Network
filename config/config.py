88d9d2b77b5fb7b0b4dd217c6b7cad5541dabc18
Sant-Thiago <thiagovieirab2b@gmail.com>
2024-08-04 21:04:37 -0300

    [FEAT] tirando arquivos de conexão

--- config/config.py
+++
@@ -1,7 +0,0 @@
-STDATABASE_CONFIG = {
-    'sqlServer': 'localhost',
-    'sqlUser': 'sa',
-    'sqlPassword': 'st@Password@2024',
-    'sqlDatabase': 'generic_database',
-    'sqlDriver': '{ODBC Driver 17 for SQL Server}'
-}
